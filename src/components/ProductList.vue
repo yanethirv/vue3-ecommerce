a6414525dfec4874be77bc439a076a41039dbcea
Yaneth Ramirez <yanethi.ramirez@gmail.com>
2024-01-26 16:49:06 -0400

    Implementacion de categorias por rutas, creacion de state products

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,33 +1,17 @@
 <script lang="ts">
 
 import ProductCard from './ProductCard.vue';
-import type { Product } from '@/model/types';
+import { useProductsStore } from '@/stores/products';
+import { mapState } from 'pinia';
 
 export default {
     // Registro local de un componente
     components: { 
         ProductCard
     },
-    // Definicion de variables de Estado
-    data() {
-        return {
-            products: [
-                { id: 1, name: 'Silla', price: 365, image: '/public/images/products/1.png', categoryId: 1 },
-                { id: 1, name: 'Mesa de Madera', price: 265, image: '/public/images/products/2.png', categoryId: 1 },
-                { id: 1, name: 'Sombrilla de Playa', price: 45, image: '/public/images/products/3.png', categoryId: 1 },
-                { id: 1, name: 'Cojines', price: 15, image: '/public/images/products/4.png', categoryId: 1 },
-                { id: 1, name: 'Flotador Flamingo', price: 35, image: '/public/images/products/5.png', categoryId: 2 },
-                { id: 1, name: 'Tv 32" Philips', price: 350, image: '/public/images/products/6.png', categoryId: 4 },
-                { id: 1, name: 'Laptop 17" HP', price: 705, image: '/public/images/products/7.png', categoryId: 4 },
-                { id: 1, name: 'Zapatos de Piel', price: 65, image: '/public/images/products/8.png', categoryId: 3 },
-                { id: 1, name: 'Vestido de Fiesta', price: 150, image: '/public/images/products/9.png', categoryId: 3 },
-                { id: 1, name: 'Kits de Juguetes', price: 45, image: '/public/images/products/10.png', categoryId: 2 },
-            ] as Product[],
-        };
-    },
-    // Metodos
-    methods: {
-        
+    computed: {
+        // Mapeamos computed properties con alguna variable de estado del store
+        ...mapState(useProductsStore, ['products'])
     }
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,30 @@
 
 import ProductList from '@/components/ProductList.vue';
 import CategoryList from '@/components/CategoryList.vue';
+import { useProductsStore } from '@/stores/products';
+
+// Funcion Externa
+function updateCategoryFromRouteParams(categoryIdParam: string|string[]) {
+  const productsStore = useProductsStore();
+  const categoryId = Number(categoryIdParam);
+  productsStore.selectCategory(categoryId);
+}
 
 export default {
   components: {
     ProductList,
     CategoryList
   },
+  // Navigation guard de vue router
+  beforeRouteEnter(to, from) {
+    //console.log('beforeRouteEnter', to.params);
+    updateCategoryFromRouteParams(to.params.categoryId);
+  },
+  // Navigation guard de vue router
+  beforeRouteUpdate(to, from) {
+    //console.log('beforeRouteUpdate', to.params);
+    updateCategoryFromRouteParams(to.params.categoryId);
+  }
 }
 </script>
 
